e187d4d1f867dc2df3c0251067b481bb4cb1c3ba
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2016-03-22 17:40:06 -0300

    Bug correction

--- drivers/tcp/driver.js
+++ drivers/tcp/driver.js
@@ -5,33 +5,34 @@ const SPORT = 4568;
 var client = dgram.createSocket('udp4');
 
 function listen(msgCallback, listenCallback) {
+    
     var server = dgram.createSocket('udp4');
-    server.bind(RPORT);
-
-    var ret = {
+    var listenObj = {
         close: function () {
-            server.close(PORT);
-        },
-        address: server.address()
-    }
+            server.close(RPORT);
+        }
+    };
 
     server.on('error', (err) =>{
-        if(listenCallback) listenCallback(err, ret);
+        if (listenCallback) listenCallback(err, listenObj);
     });
 
     server.on('listening', () =>{
-        if(listenCallback) listenCallback(null, ret);
+        if (listenCallback) listenCallback(null, listenObj);
     });
 
     server.on('message', (msg, rinfo) =>{
-        msgCallback(null, msg, rinfo, ret);
+        msgCallback(null, msg, rinfo, listenObj);
     });
 
+    server.bind(RPORT, function () {
+        listenObj.address = server.address();
+    });
 }
 
 function send(to, msg, callback) {
-    client.send(msg, to.port, to.address, () =>{
-        if(callback) callback;
+    client.send(msg, to.port, to.address, (err) => {
+        if (callback) callback(err);
     });
 }
 
--- drivers/tcp/test.js
+++ drivers/tcp/test.js
@@ -1,15 +1,21 @@
 var driver = require("./driver.js");
 
 function test(){
-    // driver.listen((err, msg, from, socket)=>{
-    //     if(err) console.log("Error listening");
-    //     else {
-    //         console.log("Message received from " + from.adress);
-    //         console.log(msg);
-    //     }
-    // });
-
-    driver.send({address: "localhost", port: 4567}, "Oi");
+    driver.listen((err, msg, from, socket) => {
+         if(err) console.log("Error listening");
+         else {
+             console.log("Message received from " + from.address + ", port: " + from.port);
+             console.log(new String(msg));
+             socket.close();
+         }
+     }, (err) => {
+     	if (!err) { 
+     		console.log("Listening!");
+     		driver.send({address: "localhost", port: 4567}, "Oi", function () {
+    			console.log("Sent!");
+    		});
+    	}
+    });
 }
 
 test();
