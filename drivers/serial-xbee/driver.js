d42c828316877f44b6336430f04c9806e9a9a02d
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2016-03-22 21:53:14 -0300

    Started xbee

---
+++ drivers/serial-xbee/driver.js
@@ -0,0 +1,14 @@
+
+var xbee_api = require('xbee-api');
+
+function listen(msgCallback, listenCallback) {
+	//TODO: listen message
+}
+
+function send(to, msg, callback) {
+	//TODO: send message
+}
+
+
+exports.listen = listen;
+exports.send = send;
