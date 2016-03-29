cf616a40e85e029a1cc199b33ed0fa87363eaa38
Henrique Rodrigues <henrique@localhost.localdomain>
2016-03-29 16:04:18 -0300

    Writing with xbee-api to send and listen data with 802.15.4

--- drivers/serial-xbee/test.js
+++ drivers/serial-xbee/test.js
@@ -1,32 +1,6 @@
 var util = require('util');
-var SerialPort = require('serialport').SerialPort;
-var xbee_api = require('xbee-api');
 
-var C = xbee_api.constants;
-
-var xbeeAPI = new xbee_api.XBeeAPI({
-    api_mode: 1
-});
-
-var serialport = new SerialPort("/dev/ttyAMA0", {
-    baudrate: 9600,
-    parser: xbeeAPI.rawParser()
-});
-
-serialport.on("open", function() {
-    setInterval(function () {
-        var frame_obj = { // AT Request to be sent to  
-            type: C.FRAME_TYPE.AT_COMMAND,
-            command: "NI",
-            commandParameter: [],
-        };
-
-        serialport.write(xbeeAPI.buildFrame(frame_obj));
-         console.log('Sent to serial port.');
-     }, 3000);
-});
-
-// All frames parsed by the XBee will be emitted here 
+// All frames parsed by the XBee will be emitted here
 xbeeAPI.on("frame_object", function(frame) {
     console.log(">>", frame);
 });
