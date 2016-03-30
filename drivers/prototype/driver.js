09b720e1e3d922229cfb085a1f9297b709f3a006
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2016-03-30 14:26:43 -0300

    added more functions to prototype

--- drivers/prototype/driver.js
+++ drivers/prototype/driver.js
@@ -36,6 +36,30 @@ Driver.prototype.send = function (to, message, callback) {
     throw Error("Not implemented");
 }
 
+/**
+    Gets the driver network address. Only need to work when "listening" was called
+    @type {Object} Network address
+*/
+Driver.prototype.getAddress = function () {
+    throw Error("Not implemented");
+}
+
+/**
+    Gets the broadcast network address. Only need to work when "listening" was called
+    @type {Object}  Broadcast network address
+*/
+Driver.prototype.getBroadcastAddress = function () {
+    throw Error("Not implemented");
+}
+
+/**
+    Compares two address
+    @type {boolean} true if address1 and adress2 are the same and false otherwise
+*/
+Driver.compareAddresses = function (address1, address2) {
+    throw Error("Not implemented");
+}
+
 exports.Driver = Driver;
 
 /**
