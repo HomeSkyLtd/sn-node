b6e1b6170496347f43ea1b6652ddc16ac6ba674e
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2016-03-30 13:48:50 -0300

    Added prototype and documentation for driver

---
+++ drivers/prototype/driver.js
@@ -0,0 +1,64 @@
+/**
+	@class
+    Driver object for a network protocol
+    @type {Object} Returns a Driver instance
+    @param {Object} [params] - An object containing parameters for the specific driver
+    @param {Driver~onInitialized} [callback] - Function to be called when the driver is initialized
+*/
+function Driver (params, callback) {
+    throw Error("not implemented")
+}
+
+/**
+    Starts listening for messages
+    @param {Driver~onMessage} msgCallback - Function to be called when a message arrives
+    @param {Driver~onListening} [listenCallback] - Function to be called when a message arrives
+*/
+Driver.prototype.listen = function (msgCallback, listenCallback) { 
+    throw Error("Not implemented");
+}
+
+/**
+	Stops listening for messages
+*/
+Driver.prototype.close = function () {
+    throw Error("Not implemented");
+}
+
+/**
+	Sends a message to address 
+  	@param {Object} to - Object containing the address object of the recipient
+  	@param {Buffer} message - Buffer containing the message to be sent
+  	@param {Driver~onSent} [callback] - Function to be called when the message was sent
+
+*/
+Driver.prototype.send = function (to, message, callback) {
+    throw Error("Not implemented");
+}
+
+exports.Driver = Driver;
+
+/**
+ * Callback used by Driver.
+ * @callback Driver~onInitialized
+ * @param {Error} error - If there is a problem initializing this will be an Error object, otherwise will be null 
+ */
+
+/**
+ * Callback used by listen.
+ * @callback Driver~onMessage
+ * @param {Buffer} message - Buffer containing the buffer received from the network
+ * @param {Object} from - Object containing the address object of the transmitter
+ */
+
+/**
+ * Callback used by listen.
+ * @callback Driver~onListening
+ * @param {Error} error - If there is a problem listening this will be an Error object, otherwise will be null 
+ */
+
+/**
+ * Callback used by send.
+ * @callback Driver~onSent
+ * @param {Error} error - If there is a problem sending this will be an Error object, otherwise will be null 
+ */ 
