6409eedaab736a6630360ef3de7c75949bcd686c
Henrique Rodrigues <henriqrodrigues@gmail.com>
2016-06-06 13:45:44 -0300

    cbor in utilities

---
+++ utilities/cbor/cbor.js
@@ -0,0 +1,12 @@
+var cbor = require('cbor');
+
+var content = {"temp": 15, "id": 1234};
+var encoded = cbor.encode(content);
+console.log("###CBOR###")
+console.log(`Length: ${encoded.length} bytes`);
+console.log(encoded);
+console.log();
+
+console.log("###JSON###");
+console.log(`Length: ${JSON.stringify(content).length} bytes`);
+console.log(JSON.stringify(content));
