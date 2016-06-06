2733d455e10af5144cd9ca5f6b1ace865a95ea44
Henrique Rodrigues <henriqrodrigues@gmail.com>
2016-06-06 13:46:41 -0300

    Correcting syntax in cbor.js

--- utilities/cbor/cbor.js
+++ utilities/cbor/cbor.js
@@ -1,8 +1,10 @@
+/*jshint esversion: 6 */
+
 var cbor = require('cbor');
 
 var content = {"temp": 15, "id": 1234};
 var encoded = cbor.encode(content);
-console.log("###CBOR###")
+console.log("###CBOR###");
 console.log(`Length: ${encoded.length} bytes`);
 console.log(encoded);
 console.log();
